4f5fc603ca1d09e92829aa091964de26bbba982e
Goktug <gokojapan5@gmail.com>
2023-07-04 19:12:13 +0300

    chore: add env and map dependencies

--- src/pages/add-location/index.tsx
+++ src/pages/add-location/index.tsx
@@ -1,5 +1,48 @@
+import { Center } from "@chakra-ui/react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
 import React from "react";
 
 export default function AddLocations() {
-  return <div>Add Location sayfasındayım</div>;
+  const containerStyle = {
+    width: "400px",
+    height: "400px",
+  };
+
+  const center = {
+    lat: 41.0801416,
+    lng: 29.0177608,
+  };
+
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_ENV_VARIABLE ?? "",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map) {
+    // This is just an example of getting and using the map instance!!! don't just blindly copy!
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map) {
+    setMap(null);
+  }, []);
+
+  return (
+    <Center>
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={14}
+          onLoad={onLoad}
+          onUnmount={onUnmount}
+        />
+      ) : null}
+    </Center>
+  );
 }
