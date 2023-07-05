1b8d310063fd0f81f256b3b60825ac5669986aae
Goktug <gokojapan5@gmail.com>
2023-07-05 03:30:05 +0300

    chore: small changes

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,18 +1,10 @@
 import {
-  Avatar,
   Box,
   Button,
-  Center,
   Flex,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuItem,
-  MenuList,
   Stack,
   useColorMode,
   useColorModeValue,
-  useDisclosure,
 } from "@chakra-ui/react";
 import React from "react";
 
@@ -23,7 +15,7 @@ export default function Header() {
   return (
     <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
       <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
-        <Box>Logo</Box>
+        <Box>Göktuğ Sevgil</Box>
 
         <Flex alignItems={"center"}>
           <Stack direction={"row"} spacing={7}>
