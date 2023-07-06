b240e4d80d20aaf7e8b79bff7ed7d8a5c960c9bc
Goktug <gokojapan5@gmail.com>
2023-07-06 20:53:18 +0300

    feat: change color mode of map depend on page color mode

--- src/pages/add-location/index.tsx
+++ src/pages/add-location/index.tsx
@@ -1,14 +1,189 @@
-import { Box, Center, Container, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Container,
+  Heading,
+  useColorMode,
+} from "@chakra-ui/react";
 import { GoogleMap, Marker, useJsApiLoader } from "@react-google-maps/api";
 import React, { useState } from "react";
 import { useLocationsStore } from "@/store/useLocationsStore";
 import Link from "next/link";
 
 export default function AddLocations() {
+  const { colorMode } = useColorMode();
+
   const containerStyle = {
     width: "100%",
     height: "400px",
   };
+
+  const stylesArray = [
+    [
+      {
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#242f3e",
+          },
+        ],
+      },
+      {
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#746855",
+          },
+        ],
+      },
+      {
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#242f3e",
+          },
+        ],
+      },
+      {
+        featureType: "administrative.locality",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#d59563",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#d59563",
+          },
+        ],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#263c3f",
+          },
+        ],
+      },
+      {
+        featureType: "poi.park",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#6b9a76",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#38414e",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#212a37",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#9ca5b3",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#746855",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "geometry.stroke",
+        stylers: [
+          {
+            color: "#1f2835",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#f3d19c",
+          },
+        ],
+      },
+      {
+        featureType: "transit",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#2f3948",
+          },
+        ],
+      },
+      {
+        featureType: "transit.station",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#d59563",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [
+          {
+            color: "#17263c",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.fill",
+        stylers: [
+          {
+            color: "#515c6d",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "labels.text.stroke",
+        stylers: [
+          {
+            color: "#17263c",
+          },
+        ],
+      },
+    ],
+    [{}],
+  ];
+
+  const [map, setMap] = React.useState(null);
 
   const [center, setCenter] = useState({
     lat: 41.015137,
@@ -17,10 +192,31 @@ export default function AddLocations() {
 
   const [color, setColor] = useState("#FF0000");
 
-  const { isLoaded } = useJsApiLoader({
+  const { isLoaded, loadError } = useJsApiLoader({
     id: "google-map-script",
     googleMapsApiKey: process.env.NEXT_PUBLIC_ENV_VARIABLE!,
   });
+
+  const onLoad = React.useCallback(function callback(map: google.maps.Map) {
+    const bounds = new window.google.maps.LatLngBounds();
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map: google.maps.Map) {
+    setMap(null);
+  }, []);
+
+  React.useEffect(() => {
+    if (map) {
+      console.log("colorMode changed, updating map");
+      map.setOptions(
+        colorMode === "light"
+          ? { styles: stylesArray[1] }
+          : { styles: stylesArray[0] }
+      );
+    }
+  }, [colorMode, map]);
 
   const { locations, addLocation } = useLocationsStore();
 
@@ -43,7 +239,9 @@ export default function AddLocations() {
   const handleColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setColor(e.target.value);
   };
-
+  if (loadError) {
+    return <div>Map cannot be loaded right now, sorry.</div>;
+  }
   return (
     <Box>
       <Center mt={8}>
@@ -58,6 +256,8 @@ export default function AddLocations() {
               center={center}
               zoom={14}
               onClick={onMapClick}
+              onLoad={onLoad}
+              onUnmount={onUnmount}
             >
               {locations.map((location) => (
                 <Marker
